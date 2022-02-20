fc8865bf86b45864ad75019dd94f47035e1966f0
john crews <jcrews@uccs.edu>
2022-02-19 23:20:25 -0700

    added write location as optional argument, updated README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # vgost-cli
 A python-based command line interface to download video game OSTs
+
+---
+### Purpose
+
+A tool written to scrape the khinsider repository of video game soundtracks
+
+### Installation guide
+
+Currently, since it's just the one script, just clone the repo and run it via python:
+
+```
+python vgost-cli.py [write-location]
+```
+
+### Future developments
+
+ * Making it so that the script can be installed and invoked as a command
+ * Extend usability with flags
+   * Play music instead of downloading?
+   * Give choice to download one song instead of album?
--- vgost-cli.py
+++ vgost-cli.py
@@ -1,4 +1,10 @@
-import requests, re, urllib
+#!/usr/bin/env python
+import os, re, requests, sys, urllib
+
+if len(sys.argv) > 1:
+    write_location = " ".join(sys.argv[1:])
+else:
+    write_location = ""
 
 site_url = "https://downloads.khinsider.com"
 
@@ -10,13 +16,13 @@ albumarr = re.findall(r"<a href=\"/game-soundtracks/album.*</a>", searchpage.tex
 
 albumlinklib = {}
 for x, album in enumerate(albumarr):
-    albumlinklib[x + 1] = [album[album.find("\">")+2:-4], "https://downloads.khinsider.com" + album[9:album.find("\">")]]
+    albumlinklib[x + 1] = [album[album.find("\">")+2:-4], site_url + album[9:album.find("\">")]]
 
 for key in albumlinklib:
-    print(key, albumlinklib[key][0])
+    print("[{}] {}".format(key, albumlinklib[key][0]))
 
 gameentry = int(input("Enter selection number: "))
-
+foldername = albumlinklib[gameentry][0]
 albumpage = requests.get(albumlinklib[int(gameentry)][1])
 
 songarr = re.findall(r"<a href=\"/game-soundtracks/album.*mp3\">[^<]*</a>", albumpage.text)
@@ -25,11 +31,13 @@ songlinklib = {}
 for x, song in enumerate(songarr):
     songlinklib[x + 1] = [song[song.find("\">")+2:-4], site_url + song[9:song.find("\">")]]
 
+os.mkdir(write_location + "/" +foldername)
+
 for key in songlinklib:
-    title = "[{}] - {}.mp3".format(key, songlinklib[key][0])
+    title = "({}) - {}.mp3".format(key, songlinklib[key][0])
     link = songlinklib[key][1]
     print("Now downloading " + title)
     dllink = re.findall(r"https://.*com/.*mp3", requests.get(link).text)
-    with open(title, "wb") as f:
+    with open(write_location + "/" + foldername + "/" + title, "wb") as f:
         with urllib.request.urlopen(dllink[0]) as r:
             f.write(r.read())
